b697ac897109cf4d9899e86f1872c802d3f81057
Joshua <joshuayuanhu@gmail.com>
2023-04-30 18:37:24 +0200

    init

---
+++ src/filemate/grades.py
@@ -0,0 +1,15 @@
+class GradeValueError(ValueError):
+    pass
+
+
+def compute_grade(average, section):
+    if section == "SG":
+        limit = 6
+    elif section == "IS":
+        limit = 7
+
+    if average < 0 or average > limit:
+        raise GradeValueError
+
+    else:
+        return round(average * 2) / 2
---
+++ tests/test_grades.py
@@ -0,0 +1,40 @@
+import filemate.grades as grades
+
+
+def test_add_subject():
+    pass
+
+
+def test_delete_subject():
+    pass
+
+
+def test_add_grade():
+    pass
+
+
+def test_delete_grade():
+    pass
+
+
+def test_compute_average():
+    pass
+
+
+def test_compute_grade():
+    assert grades.compute_grade(5.88) == 6
+
+
+def test_invalid_grade():
+# Checks whether compute_grade() raises error
+
+def test_compute_gpa():
+    pass
+
+
+def test_add_semester():
+    pass
+
+
+def test_delete_semester():
+    pass
