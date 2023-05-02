605f0be3384c33710144a10f13fdbdb1c7c9c4d8
Joshua <joshuayuanhu@gmail.com>
2023-05-02 07:52:32 +0200

    functions used to add to database created & tested, working on query functions

--- src/filemate/grades_db.py
+++ src/filemate/grades_db.py
@@ -1,4 +1,6 @@
 from peewee import *
+from datetime import date
+
 
 db = SqliteDatabase('grades.db')
 
@@ -24,15 +26,70 @@ class Semester(Model):
 class Grade(Model):
     semester = ForeignKeyField(Semester, backref="semester")
     student = ForeignKeyField(Student, backref="semester")
+    grade = FloatField()
+    weight = FloatField()
+    date = DateField()
 
     class Meta:
         database = db
 
 
-db.connect()
-db.create_tables([Student, Semester, Grade])
-joshua = Student.create(username="Joshua", school_section="SG")
-joshua.save()
-for student in Student.select():
-    print(student.username)
-db.close()
+def insert_student(username, school_section):
+    user = Student.create(username=username, school_section=school_section)
+    user.save()
+
+
+def insert_semester(username, name):
+    student = Student.select().where(Student.username == username)
+    semester = Semester.create(student=student, name=name)
+    semester.save()
+
+
+def insert_grade(username, semester_name, grade, weight, date):
+    semester = Semester.select().where((Semester.student == username) | (Semester.name == semester_name))
+    student = Student.select().where(Student.username == username)
+    grade = Grade.create(student=student, semester=semester, grade=grade, weight=weight, date=date)
+    grade.save()
+
+
+def create_students():
+    insert_student(username="bob", school_section="IS")
+    insert_student(username="tom", school_section="SG")
+    insert_student(username="ginny", school_section="SG")
+
+
+def create_semesters():
+    insert_semester("bob", "spring 2023")
+    insert_semester("bob", "fall 2023")
+    insert_semester("ginny", "spring 2022")
+    insert_semester("tom", "spring 2024")
+
+
+def create_grades():
+    insert_grade(username="bob", semester_name="spring 2023", grade=5.3, weight=1, date=date(2023, 5, 23))
+
+
+def select_student(id):
+    return Student.select().where(Student.user_id == id)
+
+
+def select_student_semesters(user_id):
+    return Semester.select().where(Semester.student == user_id)
+
+
+if __name__ == "__main__":
+    db.connect()
+    db.create_tables([Student, Semester, Grade])
+    bob = select_student(1)
+    print(bob)
+    print(select_student_semesters(1))
+
+    for student in Student.select():
+        print(student.username)
+
+    for semester in Semester.select():
+        print(semester.student.username, semester.name)
+
+    for grade in Grade.select():
+        print(grade.student.username, grade.semester.name, grade.grade, grade.weight, grade.date)
+    db.close()
