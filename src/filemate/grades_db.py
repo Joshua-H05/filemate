57b387ad9eb1bc938c7e88a5f6d74217c39f3d8b
Joshua <joshuayuanhu@gmail.com>
2023-05-02 12:23:18 +0200

    preliminary version of query functions complete

--- src/filemate/grades_db.py
+++ src/filemate/grades_db.py
@@ -26,6 +26,7 @@ class Semester(Model):
 class Grade(Model):
     semester = ForeignKeyField(Semester, backref="semester")
     student = ForeignKeyField(Student, backref="semester")
+    subject = TextField()
     grade = FloatField()
     weight = FloatField()
     date = DateField()
@@ -45,10 +46,10 @@ def insert_semester(username, name):
     semester.save()
 
 
-def insert_grade(username, semester_name, grade, weight, date):
+def insert_grade(username, semester_name, subject, grade, weight, date):
     semester = Semester.select().where((Semester.student == username) | (Semester.name == semester_name))
     student = Student.select().where(Student.username == username)
-    grade = Grade.create(student=student, semester=semester, grade=grade, weight=weight, date=date)
+    grade = Grade.create(student=student, semester=semester, subject=subject, grade=grade, weight=weight, date=date)
     grade.save()
 
 
@@ -66,31 +67,54 @@ def create_semesters():
 
 
 def create_grades():
-    insert_grade(username="bob", semester_name="spring 2023", grade=5.3, weight=1, date=date(2023, 5, 23))
+    insert_grade(username="bob", semester_name="spring 2023", subject="math", grade=5.3, weight=1, date=date(2023, 5, 23))
 
 
-def select_student(id):
-    student = Student.select().where(Student.user_id == id)
-    print(student)
+def select_student(user_id):
+    query = Student.select().where(Student.user_id == user_id)
+    results = []
+    for student in query:
+        results.append((student.user_id, student.username, student.school_section))
+    return results
 
 
 def select_student_semesters(user_id):
-    return Semester.select().where(Semester.student == user_id)
+    semesters = Semester.select().where(Semester.student == user_id)
+    results = []
+    for semester in semesters:
+        results.append((semester.semester_id, semester.student, semester.name))
+
+    return results
+
+
+def select_student_semester_grades(user_id, semester):
+    grades = Grade.select().where((Grade.student == user_id) | (Grade.semester == semester))
+    results = []
+    for grade in grades:
+        results.append((grade.subject, grade.grade, grade.weight, grade.date))
+
+    return results
 
 
 if __name__ == "__main__":
     db.connect()
     db.create_tables([Student, Semester, Grade])
-    bob = select_student(1)
-    select_student(1)
-    print(select_student_semesters(1))
+    """create_students()
+    create_semesters()
+    create_grades()"""
+    student = select_student(user_id=1)
+    print(student)
+    semester = select_student_semesters(user_id=1)
+    print(semester)
+    grades = select_student_semester_grades(user_id=1, semester=1)
+    print(grades)
 
-    for student in Student.select():
+    """for student in Student.select():
         print(student.username)
 
     for semester in Semester.select():
         print(semester.student.username, semester.name)
 
     for grade in Grade.select():
-        print(grade.student.username, grade.semester.name, grade.grade, grade.weight, grade.date)
+        print(grade.student.username, grade.semester.name, grade.subject, grade.grade, grade.weight, grade.date)"""
     db.close()
