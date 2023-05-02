bdd62a9f1c8af0c36e3d46d586ce4d3fbd51e165
Joshua <joshuayuanhu@gmail.com>
2023-05-02 12:23:51 +0200

    preliminary version of query functions complete

--- src/filemate/grades_db.py
+++ src/filemate/grades_db.py
@@ -108,7 +108,6 @@ if __name__ == "__main__":
     print(semester)
     grades = select_student_semester_grades(user_id=1, semester=1)
     print(grades)
-
     """for student in Student.select():
         print(student.username)
 
